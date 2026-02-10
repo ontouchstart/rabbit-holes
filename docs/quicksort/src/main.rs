71686fd3dd346b50156afe75367b43756030ed28
Sam Liu <ontouchstart@gmail.com>
2026-02-09 19:02:42 -0500

    PARTITION by hand

--- docs/quicksort/src/main.rs
+++ docs/quicksort/src/main.rs
@@ -1,6 +1,9 @@
 #![doc = include_str!("../README.md")]
 #![doc = include_str!("../algorithm-63.md")]
 #![doc = include_str!("../algorithm-64.md")]
+#![doc = include_str!("../partition.md")]
+#![doc = include_str!("../c.md")]
+
 //! ## Implementation in Rust
 //! To implement quicksort, we need [rand]
 //! ```toml
