38c0373e483d8b77becca23a9ce5910d3b556dab
Sam Liu <ontouchstart@gmail.com>
2026-02-24 13:04:47 -0500

    bash: Missing: command not found

--- missing/not-found.rs
+++ missing/not-found.rs
@@ -2,3 +2,12 @@
 //! % Missing Semester
 //! zsh: command not found: Missing
 //! ```
+//! ```zsh
+//! % bash
+//!
+//! The default interactive shell is now zsh.
+//! To update your account to use zsh, please run `chsh -s /bin/zsh`.
+//! For more details, please visit https://support.apple.com/kb/HT208050.
+//! bash-3.2$ Missing Semester
+//! bash: Missing: command not found
+//! ```
