87a49cd4d1d362a43b8a9a0a4317b28376b94c5e
Sam Liu <ontouchstart@gmail.com>
2026-02-24 12:59:52 -0500

    make -C missing

--- missing/missing.rs
+++ missing/missing.rs
@@ -1,1 +1,19 @@
 #![doc = include_str!("README.md")]
+//! # How this doc is generated
+//! `missing/Makefile`
+//! ```makefile
+#![doc = include_str!("Makefile")]
+//! ```
+//! You can run following shell command from the root level
+//! ```sh
+//! make -C missing
+//! ```
+//! RTFM:
+//! ```text
+//! make --help
+//! Usage: make [options] [target] ...
+//! Options:
+//! ...
+//!  -C DIRECTORY, --directory=DIRECTORY
+//!                              Change to DIRECTORY before doing anything.
+//! ```
