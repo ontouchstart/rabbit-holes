b6949b695926520ca0b9950dfe415843957689e1
Sam Liu <ontouchstart@gmail.com>
2026-02-27 11:45:02 -0500

    fix make -C ggml-gpt-oss-20b-GGUF run

--- ggml-gpt-oss-20b-GGUF/ggml-gpt-oss-20b-GGUF-hello-c.rs
+++ ggml-gpt-oss-20b-GGUF/ggml-gpt-oss-20b-GGUF-hello-c.rs
@@ -1,4 +1,17 @@
 #![doc(html_playground_url = "https://play.rust-lang.org/")]
+//! ```sh
+//! ../llama-cpp/ggml-org/llama.cpp/build/bin/llama-completion -hf ggml-org/gpt-oss-20b-GGUF -sys "You will help me     write rust programs without Cargo."
+//! ```
+//! # System prompt
+//!
+//! <|start|>system<|message|>You will help me write rust programs without Cargo.<|end|>
+//!
+//! # Prompt
+//! Write a rust program that create a hello world in C, save it as a file hello.c, compile and run it in child process.
+//!
+//! # Session log
+//! [ggml-gpt-oss-20b-GGUF-hello-c-llm.log](ggml-gpt-oss-20b-GGUF-hello-c-llm.log)
+//!
 //! # Run the code in playground
 //! ```
 #![doc = include_str!("hello-c.rs")]
@@ -9,5 +22,3 @@
 #![doc = include_str!("ggml-gpt-oss-20b-GGUF-hello-c.log")]
 //! ```
 //!
-//! # LLM log
-//! [ggml-gpt-oss-20b-GGUF-hello-c-llm.log](ggml-gpt-oss-20b-GGUF-hello-c-llm.log)
