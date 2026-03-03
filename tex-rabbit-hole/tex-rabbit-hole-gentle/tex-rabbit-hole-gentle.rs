0be9eb5577cb372102bee6ccb209e0ab76725c99
Sam Liu <ontouchstart@gmail.com>
2026-03-03 10:42:18 -0500

    tex-rabbit-hole-gentle

---
+++ tex-rabbit-hole/tex-rabbit-hole-gentle/tex-rabbit-hole-gentle.rs
@@ -0,0 +1,11 @@
+//! # A Gentle Introduction to TeX
+//! //! [gentle.pdf](gentle.pdf)
+//! ```makefile
+#! [doc = include_str!("Makefile")]
+//! ```
+//!
+//! `/Library/TeX/Documentation/texmf-dist-doc/plain/gentle/gentle.tex`
+//! ```tex
+#! [doc = include_str!("gentle/gentle.tex")]
+//! ```
+//!
