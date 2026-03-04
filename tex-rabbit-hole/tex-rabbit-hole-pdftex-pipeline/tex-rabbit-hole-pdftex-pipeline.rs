32e76df8789ed86edeb75c26c0dce7802c290e7a
Sam Liu <ontouchstart@gmail.com>
2026-03-04 13:17:57 -0500

    diagram.tex

--- tex-rabbit-hole/tex-rabbit-hole-pdftex-pipeline/tex-rabbit-hole-pdftex-pipeline.rs
+++ tex-rabbit-hole/tex-rabbit-hole-pdftex-pipeline/tex-rabbit-hole-pdftex-pipeline.rs
@@ -1,11 +1,14 @@
 //! # pdftex pipeline
 //! [main.pdf](main.pdf)
+//! `main.tex`
 //! ```tex
 #! [doc = include_str!("main.tex")]
 //! ```
+//! `diagram.tex`
 //! ```tex
 #! [doc = include_str!("diagram.tex")]
-//!
+//! ```
+//! `Makefile`
 //! ```makefile
 #! [doc = include_str!("Makefile")]
 //! ```
