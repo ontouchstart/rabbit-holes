1b257504146282f7346332fa638b22976eb646e9
Sam Liu <ontouchstart@gmail.com>
2026-03-04 13:27:01 -0500

    makebox.tex

--- tex-rabbit-hole/tex-rabbit-hole-pdftex-pipeline/tex-rabbit-hole-pdftex-pipeline.rs
+++ tex-rabbit-hole/tex-rabbit-hole-pdftex-pipeline/tex-rabbit-hole-pdftex-pipeline.rs
@@ -4,6 +4,10 @@
 //! ```tex
 #! [doc = include_str!("main.tex")]
 //! ```
+//! `makebox.tex`
+//! ```tex
+#! [doc = include_str!("makebox.tex")]
+//! ```
 //! `diagram.tex`
 //! ```tex
 #! [doc = include_str!("diagram.tex")]
