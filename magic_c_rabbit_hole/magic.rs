dd1f5f4088644308794b98c6ced8838420647d90
Sam Liu <ontouchstart@gmail.com>
2026-03-11 19:53:08 -0400

    Update magic.rs

--- magic_c_rabbit_hole/magic.rs
+++ magic_c_rabbit_hole/magic.rs
@@ -3,7 +3,7 @@ extern "C" {
 }
 
 fn main() {
-    // Call the C function
-    let status = unsafe { magic_answer() };
-    std::process::exit(status); // Exit with the returned value (42)
+    unsafe {
+        std::process::exit(magic_answer()); // Exit with the returned value (42)
+    }
 }
