770f1640f5a53df98c6c2d29978f8aa32e80c272
Sam Liu <ontouchstart@gmail.com>
2026-03-30 11:44:22 -0400

    https://ontouchstart.github.io/.pi/output/--home--/2026-03-30T15-35-24-157Z_e4fe3bd7-329b-4f6e-8592-500a83c6093e.html

---
+++ home/packages/toolchain/dist/index.d.ts
@@ -0,0 +1,2 @@
+export declare function greet(name: string): string;
+export declare const PI: number;
---
+++ home/packages/toolchain/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PI = void 0;
+exports.greet = greet;
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+exports.PI = Math.PI;
