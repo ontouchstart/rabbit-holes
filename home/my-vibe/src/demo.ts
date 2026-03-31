fde05eee8a6637e25be00f039260a1df44c8b9e8
Sam Liu <ontouchstart@gmail.com>
2026-03-31 17:14:09 -0400

    revert src/demo.ts

--- home/my-vibe/src/demo.ts
+++ home/my-vibe/src/demo.ts
@@ -1,23 +1,5 @@
+import { helloRust } from './rust';
+import { helloPython } from './python';
 
-(async () => {
-  const { createAgentSession } = await import('@mariozechner/pi-coding-agent');
-  const { session } = await createAgentSession();
-
-  session.subscribe((event) => {
-    if (
-      event.type === 'message_update' &&
-      event.assistantMessageEvent.type === 'text_delta'
-    ) {
-      process.stdout.write(event.assistantMessageEvent.delta);
-    }
-  });
-
-  await session.prompt('What files are in the current directory?');
-  session.state.messages.forEach((msg) => {
-    console.log(msg);
-  });
-  console.log();
-})();
-
-export {};
-
+console.log('Rust:', helloRust());
+console.log('Python:', helloPython());
